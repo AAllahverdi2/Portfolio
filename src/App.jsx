dd4697f629afb98e5a7b19be6943143254067141
AAllahverdi2 <agamaliyevallahverdi2@gmail.com>
2024-10-15 21:27:52 +0400

    added text

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import logo from '../src/assets/Allahverdilogo.png'; // Uzantıyı ve dosya yolu
 const App = () => {
   return (
     <div>
+      <h1>Yaxsi Oglan Allahverdi</h1>
       <img src={logo} alt="Allahverdi Logo" />
     </div>
   );
