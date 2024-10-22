6f94008e751d66bc0ae51b25375af75afe2a316b
AAllahverdi2 <agamaliyevallahverdi2@gmail.com>
2024-10-22 20:22:12 +0400

    added new file

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div style={{zIndex:"200000000000030000000000"}}>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni iure, delectus corrupti, assumenda reiciendis necessitatibus a vel totam iusto voluptas tenetur, quia quos fugiat soluta omnis nisi facere. Quis suscipit in tempora vel voluptate modi ea, minima, quibusdam dolores aliquam enim est? Natus repellat officia amet, nesciunt deleniti blanditiis eaque, est maiores explicabo enim autem consectetur odio debitis ipsam aliquam at ea? Nulla, ab molestiae aut harum odio ducimus eligendi quo tempore, molestias natus ipsam magnam sunt maiores numquam voluptates ipsum dolorum delectus deleniti consectetur ratione tenetur, unde alias. Laudantium quo sed error quae facilis saepe qui. Dicta, asperiores porro.
     </div>
   )
 }
--- src/Pages/MainRoot.jsx
+++ src/Pages/MainRoot.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import Header from '../Layout/Header'
-import { Outlet  } from "react-router-dom";
+import React from 'react';
+import Header from '../Layout/Header';
+import { Outlet } from "react-router-dom";
 import Footer from '../Layout/Footer';
+import './MainRoot.scss'; // CSS faylını daxil edin
+
 const MainRoot = () => {
   return (
-    <div>
-        <Header/>
+    <div className="main-root">
+      <Header/>
+      <div className="content">
         <Outlet/>
-        <Footer/>
+      </div>
+      <Footer/>
     </div>
-  )
+  );
 }
 
-export default MainRoot
+export default MainRoot;
