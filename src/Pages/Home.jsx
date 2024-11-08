fb269bd9aff51ff2be75345976b1d74e290fedd9
AAllahverdi2 <agamaliyevallahverdi2@gmail.com>
2024-11-08 18:33:34 +0400

    deleted section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import HomeSection1 from '../companents/Home/HomeSection1'
 import './Home.scss'
-import Section1 from '../companents/Skills/Section1'
 const Home = () => {
   return (
     <div className='home__all'>
       <HomeSection1/>
-      <EducationSection/>
     </div>
   )
 }
